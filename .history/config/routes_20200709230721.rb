c0c0d875cfd650af754b6100507f055dac87744f
Raymond Oluoch <rasholuoch@gmail.com>
2020-07-10 01:05:15 +0300

    forms, database and sending emails

---
+++ .history/config/routes_20200709230721.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact-us', to: 'contacts#new'
+end
---
+++ .history/db/migrate/20200709162909_create_contacts_20200709192908.rb
@@ -0,0 +1,6 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+    end
+  end
+end
