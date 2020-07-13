0f99636d6a5ab4826ddde0e007f40bf304ab3ca0
Raymond Oluoch <rasholuoch@gmail.com>
2020-07-14 00:49:21 +0300

    configure mailer settings

--- .history/config/environment_20200714004755.rb
+++ .history/config/environment_20200714004755.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'desolate-retreat-72605.herokuapp.com/',
+  :domain         => 'shrouded-fortress-66409.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
